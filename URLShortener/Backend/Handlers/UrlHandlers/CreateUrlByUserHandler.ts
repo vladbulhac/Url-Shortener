3e60af0c1246637687a9f1a6c2ef57d5a888d80b
vladbulhac <48600549+vladbulhac@users.noreply.github.com>
2020-10-18 17:40:06 +0300

    + fixed UrlRepository's method GetByIdentifier by checking if a short url or a long url matches the given parameter

--- URLShortener/Backend/Handlers/UrlHandlers/CreateUrlByUserHandler.ts
+++ URLShortener/Backend/Handlers/UrlHandlers/CreateUrlByUserHandler.ts
@@ -14,43 +14,36 @@ export async function CreateUrlByUserHandler(
   CacheService: ICacheService,
   customUrl: string | undefined | null
 ): Promise<string> {
-  
-  
   let existingUrl: Url | null;
   if (customUrl) existingUrl = await UrlRepository.GetByIdentifier(customUrl);
   else existingUrl = await UrlRepository.GetByIdentifier(url);
 
   if (existingUrl) {
-      if(existingUrl.isActive===true && customUrl)
-         throw new ConflictError("This url already exists");
-    else
-        if(existingUrl.isActive===false)
-            {
-              await UrlRepository.SetActive(String(existingUrl._id));
-     return existingUrl.shortUrl;
-            }
-    else
-        if(existingUrl.isActive===true)
-            return existingUrl.shortUrl;
-   }
-
-    let newUrl: any = {
-      trueUrl: url,
-      accessNumber: 1,
-      extendedTTL: true,
-    };
-
-    let savedUrl:Url;
-    if (customUrl) {
-      newUrl["shortUrl"] = customUrl;
-      savedUrl = await UrlRepository.Add(newUrl);
-      await UserRepository.UpdateCustomUrls(userId, savedUrl);
-    } else {
-      const shortUrl: string = UrlConversionService.ShortUrl(url);
-      newUrl["shortUrl"] = shortUrl;
-     savedUrl= await UrlRepository.Add(newUrl);
-    }
-    CacheService.Add(savedUrl.shortUrl,JSON.stringify(savedUrl));
-
-    return newUrl.shortUrl;
+    if (existingUrl.isActive === true && customUrl)
+      throw new ConflictError("This url already exists");
+    else if (existingUrl.isActive === false) {
+      await UrlRepository.SetActive(String(existingUrl._id));
+      return existingUrl.shortUrl;
+    } else if (existingUrl.isActive === true) return existingUrl.shortUrl;
   }
+
+  let newUrl: any = {
+    trueUrl: url,
+    accessNumber: 1,
+    extendedTTL: true,
+  };
+
+  let savedUrl: Url;
+  if (customUrl) {
+    newUrl["shortUrl"] = customUrl;
+    savedUrl = await UrlRepository.Add(newUrl);
+    await UserRepository.UpdateCustomUrls(userId, savedUrl);
+  } else {
+    const shortUrl: string = UrlConversionService.ShortUrl(url);
+    newUrl["shortUrl"] = shortUrl;
+    savedUrl = await UrlRepository.Add(newUrl);
+  }
+  CacheService.Add(savedUrl.shortUrl, JSON.stringify(savedUrl));
+
+  return newUrl.shortUrl;
+}
