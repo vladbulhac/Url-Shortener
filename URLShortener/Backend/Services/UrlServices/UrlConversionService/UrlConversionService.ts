08dd8bad452dec69ef077d50a674da04995a7894
vladbulhac <48600549+vladbulhac@users.noreply.github.com>
2021-01-06 16:11:03 +0200

    added the characters used in the url conversion algorithm to the .env file, changed the data structure that stores the characters used in url conversion from Map to Array, removed the method BuildIndexToCharsMap()

--- URLShortener/Backend/Services/UrlServices/UrlConversionService/UrlConversionService.ts
+++ URLShortener/Backend/Services/UrlServices/UrlConversionService/UrlConversionService.ts
@@ -2,17 +2,17 @@ import md5 from "md5";
 import { IUrlConversionService } from "./IUrlConversionService";
 
 export class UrlConversionService implements IUrlConversionService {
-  private IndexToCharsMap!: Map<number, string>;
+  private UrlConversionCharacters:string[];
 
   constructor() {
-    this.BuildIndexToCharsMap();
+    this.UrlConversionCharacters=process.env.URL_CONVERSIONCHARACTERS!.split('');
   }
 
   public ShortUrl(longUrl:string):string{
     let hashedUrl:string=md5(longUrl).slice(0,7);
     let hashedUrl_number:number=parseInt(hashedUrl,16);
 
-    let totalCharacters: number = this.IndexToCharsMap.size;
+    let totalCharacters: number = this.UrlConversionCharacters.length;
     let baseConversionDigits:number[]=[];
     while (hashedUrl_number != 0) {
       baseConversionDigits.push( hashedUrl_number % totalCharacters);
@@ -27,29 +27,8 @@ export class UrlConversionService implements IUrlConversionService {
     let newUrl: string = "";
     for(let number of baseConversionDigits)
     {
-      newUrl+=this.IndexToCharsMap.get(number);
+      newUrl+=this.UrlConversionCharacters[number];
     }
     return newUrl;
-  }
-
-
-  private BuildIndexToCharsMap(): void {
-    this.IndexToCharsMap = new Map<number, string>();
-
-    let index: number = 0;
-    let counter: number = 0;
-    while (true) {
-      if (counter <= 25)
-        this.IndexToCharsMap.set(counter, String.fromCharCode(65 + index));
-      else if (counter <= 51) {
-        if (index === 27) index = 0;
-        this.IndexToCharsMap.set(counter, String.fromCharCode(97 + index));
-      } else if (counter <= 61) {
-        if (index === 27) index = 0;
-        this.IndexToCharsMap.set(counter, String.fromCharCode(48 + index));
-      } else break;
-      counter++;
-      index++;
-    }
   }
 }
